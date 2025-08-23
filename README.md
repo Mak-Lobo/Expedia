15f34882827a6d9fcd2c1292f8da870f79ca6e87
Mak-Lobo <markknjoroge03@gmail.com>
2025-08-24 01:13:22 +0300

    Connected to the database. Installation of relevant packages

---
+++ README.md
@@ -0,0 +1,8 @@
+#  ***EXPEDIA***
+
+- A simple application that lets one book a flight.
+- This project will consist of two parts:
+  1. ###### Frontend:
+     - This will be done using the Flutter framework, working on either mobile, web, or desktop platforms
+  2. ###### Backend:
+     - This will be done using the FastAPI framework, running on a local server.
---
+++ backend/db_config.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "expedia",
+    "user": "my_bench",
+    "password": "Makomani@123"
+}
+
+# connecting to the database
+def connect_db():
+    try:
+        connection = mysql.connector.connect(**DB_CONFIG)
+        if connection.is_connected():
+            print("Connected to MySQL database")
+            return connection
+    except Error as e:
+        print(f"Cannot connect to the database. \nError: {e}")
+
+connect_db()
